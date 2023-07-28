92cb269c047fec4085601689cdb69b55316d8a41
Arin <arinmandal40@gmail.com>
2023-07-28 23:21:56 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
 const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
 // console.log(heading) this an object that return to the console.
 const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -26,13 +30,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 </div> */}
 
 const parent = React.createElement("div", { id: "parent" },
-  [React.createElement("div", { id: "child" }, React.createElement("h1", {}, "Hello H1 from First Child Div"
-  ))], [React.createElement("div", { id: "child2" }, React.createElement("h2", {}, "Hello H2 from Second Child Div"
+  [React.createElement("div", { id: "child" }, React.createElement("h1", {}, "This is namaste react 🚀"
+  ))], [React.createElement("div", { id: "child2" }, React.createElement("h2", {}, "Hello Hello"
   ))]
 
 )
-
-
-
 
 root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Namaste React 🙏
+# Namaste React 🙏
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Image Optimization
+- Minification file
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Diagnosis
+- Error handling
+- HTTPs
+- Tree Shaking
+- Different build and production (we need to remove "main":"App.js" from package.json)
