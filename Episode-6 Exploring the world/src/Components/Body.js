446f0f93b78929758dfadc9fab87aab79f8c71b6
Arin <arinmandal40@gmail.com>
2023-08-13 01:26:07 +0530

    fetched data using useEffects() Hooks

--- Episode-6 Exploring the world/src/Components/Body.js
+++ Episode-6 Exploring the world/src/Components/Body.js
@@ -1,10 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantList from "../utils/mockData"
-import { useState } from "react";
+import ShimmerUI from "./ShimmerUI";
+// import restaurantList from "../utils/mockData"
+import { useState, useEffect } from "react";
 const Body = () => {
   // State Variable - super powerful variable
-  const [RestaurantList, setRestaurantList] = useState(restaurantList);
-  return (
+  const [RestaurantList, setRestaurantList] = useState([]);
+
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch(`https://www.swiggy.com/dapi/restaurants/search/v3?lat=22.572646&lng=88.36389500000001&str=Restaurants%20in%20kolkata&trackingId=undefined&submitAction=ENTER&queryUniqueId=d8d50207-c049-6902-2c11-98faf582567a`);
+
+    const json = await data.json();
+    // Optional Chaining
+    setRestaurantList(json?.data?.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    )
+  }
+
+  // Conditional Rendering
+  // if (RestaurantList.length === 0) {
+  //   return (<ShimmerUI />)
+  // }
+
+  return RestaurantList.length === 0 ? <ShimmerUI/> :(
     <div className='main'>
       <div className='search-container'>
         <input type='search' placeholder='Search your favorite food' />
