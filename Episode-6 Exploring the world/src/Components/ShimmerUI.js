68e0f03b33f5c73e638b23d3afad7144bffbc76f
Arin <arinmandal40@gmail.com>
2023-08-13 01:24:25 +0530

    ShimmerUI component added..

---
+++ Episode-6 Exploring the world/src/Components/ShimmerUI.js
@@ -0,0 +1,58 @@
+const ShimmerUI = () => {
+  return <div className="res-container">
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+    <div className="card-container">
+      <div className="card"></div>
+    </div>
+  </div>
+}
+
+
+export default ShimmerUI;
