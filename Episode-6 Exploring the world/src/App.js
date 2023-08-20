836caf59d0fc1664a1963b3a14c7f142288d6ff3
Arin <arinmandal40@gmail.com>
2023-08-20 21:01:31 +0530

    added routing functionality into our app

--- Episode-6 Exploring the world/src/App.js
+++ Episode-6 Exploring the world/src/App.js
@@ -4,17 +4,44 @@ import "./styles/styles.css"
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Body from "./Components/Body";
-
+import About from "./Components/About";
+import Error from "./Components/Error"
+import {
+  createBrowserRouter,
+  RouterProvider, Outlet
+} from "react-router-dom";
+import Contact from "./Components/Contact";
 // *! not using key(not acceptable in react it will give you an error) <<< index as key <<< using unique id 
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
-      <Body/>
-      <Footer />
+      <Outlet />
     </div>
   );
 };
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ],
+    errorElement: <Error />
+  },
+
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
