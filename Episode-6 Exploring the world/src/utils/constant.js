249693a9c0e36cd573198a73e7fb636b3a9d0daf
Arin <arinmandal40@gmail.com>
2023-08-21 02:34:07 +0530

    new MENU_API

--- Episode-6 Exploring the world/src/utils/constant.js
+++ Episode-6 Exploring the world/src/utils/constant.js
@@ -1,1 +1,4 @@
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+
+export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId="
