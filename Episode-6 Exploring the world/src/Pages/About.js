6d1371e9e1b54b119f6d75ecf8d377452f7e272a
Arin <arinmandal40@gmail.com>
2023-08-23 17:15:46 +0530

    About Pages converted into Class based component

--- Episode-6 Exploring the world/src/Pages/About.js
+++ Episode-6 Exploring the world/src/Pages/About.js
@@ -1,8 +1,27 @@
+import React, { Component } from "react"
+import UserClass from "../Components/UserClass"
 
-const About = () => {
-  return (
-    <div><h1>About</h1></div>
-  )
+class About extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor")
+  }
+
+  componentDidMount() {
+    console.log("Parent componentDidMount");
+  }
+  render() {
+
+    console.log("Parent Render")
+    return (
+      <div className="AboutPage">
+        <h1>About</h1>
+        <UserClass name={"First"} location={"Tamluk"} />
+      </div>
+    )
+  }
 }
+
 
 export default About
