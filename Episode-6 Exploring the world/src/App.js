5f3d5cf4227154d2c3601bc76dfe5faa36e235ca
Arin <arinmandal40@gmail.com>
2023-08-26 00:02:16 +0530

    Added Lazy loading

--- Episode-6 Exploring the world/src/App.js
+++ Episode-6 Exploring the world/src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/styles.css"
 import Header from "./Components/Header";
+import Footer from "./Components/Footer"
 import Body from "./Components/Body";
 import About from "./Pages/About";
 import Error from "./Pages/Error";
 import RestaurantMenu from "./Pages/RestaurantMenu"
+// import Grocery from "./Components/Grocery";
 import {
   createBrowserRouter,
   RouterProvider, Outlet
 } from "react-router-dom";
 import Contact from "./Pages/Contact";
-// *! not using key(not acceptable in react it will give you an error) <<< index as key <<< using unique id 
+// *! not using key(not acceptable in react it will give you an error) <<< index as key <<< using unique id
+
+// Lazy Loading 
+const Grocery = lazy(() => import("./Components/Grocery"))
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
+
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -29,6 +36,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/",
         element: <Body />
+      },
+      {
+        path: "/grocery",
+        element: <Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense>
       },
       {
         path: "/about",
