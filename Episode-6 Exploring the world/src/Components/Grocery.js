e07955aeca430282e1f6067f1afa1814f7de874e
Arin <arinmandal40@gmail.com>
2023-08-26 00:09:06 +0530

    Optimized Grocery component with Lazy Loading

---
+++ Episode-6 Exploring the world/src/Components/Grocery.js
@@ -0,0 +1,14 @@
+const Grocery = () => {
+  return (
+    <div className="main">
+      <div className="grocery-page">
+        <h1>
+          Grocery
+        </h1>
+        <p>This is Foodie Express Grocery page. </p>
+      </div>
+    </div>
+  )
+}
+
+export default Grocery;
