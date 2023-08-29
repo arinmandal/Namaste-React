669214f7c9bcdcac6772ab11eadd87b840f3b8c1
Arin <arinmandal40@gmail.com>
2023-08-30 01:13:32 +0530

    Creating an accordion component from scratch

---
+++ src/Components/RestaurantCategoriesAccordion.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Itemlist } from './ItemsList'
+import { useState } from 'react'
+const RestaurantCategoriesAccordion = ({ data }) => {
+  // console.log(data)
+
+  const [showItems, setShowItems] = useState(false);
+
+  const handleClick = () => {
+    setShowItems(!showItems);
+  }
+  const { title
+    , itemCards
+  } = data.card.card;
+  // console.log(itemCards)
+  return (
+    <div className='my-4 mx-auto shadow-lg'>
+      {/* { Accordion header} */}
+      {/* Accordion body */}
+      <div className='accordion'>
+        <div className='Accordion_Header flex justify-between px-9  py-4 cursor-pointer' onClick={handleClick}>
+          <span className='font-bold tracking-wide'>{title}({itemCards.length})</span>
+          <span>🔽</span>
+        </div>
+        {showItems && <Itemlist items={itemCards} />}
+      </div>
+    </div>
+  )
+}
+
+
+export default RestaurantCategoriesAccordion
