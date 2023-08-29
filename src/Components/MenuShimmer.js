d9d6cf235bc964e3d0d2c962b88fb6f98b2c268d
Arin <arinmandal40@gmail.com>
2023-08-29 21:33:58 +0530

    Added Shimmer UI for Restaurant Menu

---
+++ src/Components/MenuShimmer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MenuShimmer = () => {
+  return (
+    <div className='h-screen w-[60%] mx-auto  border-2 solid-black'>
+      <h1 className='text-center'>
+        loading...
+      </h1>
+    </div>
+  )
+}
+
+export default MenuShimmer
