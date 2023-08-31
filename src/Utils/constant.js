9371965f788403782f15d280992cc100e8171cf6
Arin <arinmandal40@gmail.com>
2023-08-31 16:05:00 +0530

    Added new API

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -3,7 +3,8 @@ export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,
 //! Default Restaurant
 export const RESTAURANT = "https://www.swiggy.com/dapi/restaurants/search/v3?lat=22.572646&lng=88.36389500000001&str=Restaurants%20in%20kolkata&trackingId=undefined&submitAction=ENTER&queryUniqueId=d8d50207-c049-6902-2c11-98faf582567a";
 
-
+// ! New Restaurant 
+export const NEW_RES = "https://www.swiggy.com/mapi/homepage/getCards?lat=22.572646&lng=88.36389500000001"
 
 //! Dynamic Routing
 export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId="
