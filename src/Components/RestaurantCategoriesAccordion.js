55b40982843034f8786e5349cf3baea05b8594c9
Arin <arinmandal40@gmail.com>
2023-08-31 16:02:46 +0530

    uncontrolled component , lifting state up

--- src/Components/RestaurantCategoriesAccordion.js
+++ src/Components/RestaurantCategoriesAccordion.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { Itemlist } from './ItemsList'
 import { useState } from 'react'
-const RestaurantCategoriesAccordion = ({ data }) => {
+const RestaurantCategoriesAccordion = ({ data, showItems,setShowIndex }) => {
   // console.log(data)
 
-  const [showItems, setShowItems] = useState(false);
+  // const [showItems, setShowItems] = useState(false);
 
   const handleClick = () => {
-    setShowItems(!showItems);
+    // setShowItems(!showItems);
+    setShowIndex();
   }
   const { title
     , itemCards
   } = data.card.card;
   // console.log(itemCards)
   return (
-    <div className='my-4 mx-auto shadow-lg'>
+    <div className='my-4 mx-auto shadow-lg '>
       {/* { Accordion header} */}
       {/* Accordion body */}
       <div className='accordion'>
-        <div className='Accordion_Header flex justify-between px-9  py-4 cursor-pointer' onClick={handleClick}>
+        <div className='Accordion_Header flex justify-between px-9  py-4 cursor-pointer ' onClick={handleClick}>
           <span className='font-bold tracking-wide'>{title}({itemCards.length})</span>
           <span>🔽</span>
         </div>
