6fb314b42d06bdf2711d4f8cd54dbd83d9a4eadc
Arin <arinmandal40@gmail.com>
2023-09-11 00:22:42 +0530

    Loading..

--- src/Components/MenuShimmer.js
+++ src/Components/MenuShimmer.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { DotPulse } from '@uiball/loaders'
+// import { DotPulse } from '@uiball/loaders'
 const MenuShimmer = () => {
   return (
     <div className='h-screen w-[60%] mx-auto  border-2 solid-black flex justify-center items-center'>
-        <DotPulse
+        {/* <DotPulse
           size={40}
           speed={1.3}
           color="black"
-        />
+        /> */}
+      <h1>Loading ...</h1>
     </div>
   )
 }
 
-export default MenuShimmer
+export default MenuShimmer;
